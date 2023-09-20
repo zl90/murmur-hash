0407c4b3599daee025769b4cbb0ce8c6c44760c1
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-20 20:00:11 +1000

    Add shared library

---
+++ src/murmur.h
@@ -0,0 +1,10 @@
+#ifndef MURMUR_H
+#define MURMUR_H
+
+#include <string>
+
+uint32_t leftRotate(uint32_t, uint32_t);
+uint32_t multiply(uint32_t, uint32_t);
+uint32_t murmur3_32(std::string, uint32_t);
+
+#endif
